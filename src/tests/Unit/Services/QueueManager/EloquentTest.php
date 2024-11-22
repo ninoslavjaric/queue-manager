4d18219992195f32192794625e23453a27192437
ninoslavjaric <jaricninoslav@gmail.com>
2024-11-22 02:38:34 +0100

    one test case

--- src/tests/Unit/Services/QueueManager/EloquentTest.php
+++ src/tests/Unit/Services/QueueManager/EloquentTest.php
@@ -2,10 +2,12 @@
 
 namespace Nino\CustomQueueLaravelTests\Unit\Services\QueueManager;
 
+use Illuminate\Config\Repository;
 use Nino\CustomQueueLaravel\Models\CustomQueueTask;
 use Nino\CustomQueueLaravel\Services\Logger\DefaultLogger;
 use Nino\CustomQueueLaravel\Services\Logger\ErrorLogger;
 use Nino\CustomQueueLaravel\Services\QueueManager\Eloquent;
+use Nino\CustomQueueLaravel\Services\QueueManagerPayloadException;
 use Nino\CustomQueueLaravel\Services\QueueManagerPayloadValidator;
 use PHPUnit\Framework\TestCase;
 use Mockery;
@@ -13,23 +15,38 @@ use Mockery;
 class EloquentTest extends TestCase
 {
     private Eloquent $queueManager;
+    private ErrorLogger $errorLogger;
+    private DefaultLogger $defaultLogger;
+    private QueueManagerPayloadValidator $validator;
 
     protected function setUp(): void
     {
         parent::setUp();
 
+        $this->errorLogger = $this->createMock(ErrorLogger::class);
+        $this->defaultLogger = $this->createMock(DefaultLogger::class);
+        $this->validator = $this->createMock(QueueManagerPayloadValidator::class);
+
         $this->queueManager = new Eloquent(
-            errorLogger: Mockery::mock(ErrorLogger::class),
-            defaultLogger: Mockery::mock(DefaultLogger::class),
-            validator: Mockery::mock(QueueManagerPayloadValidator::class),
+            errorLogger: $this->errorLogger,
+            defaultLogger: $this->defaultLogger,
+            validator: $this->validator,
         );
 
         $this->queueManager->setModel(Mockery::mock(CustomQueueTask::class));
     }
 
-    protected function tearDown(): void
+
+    public function test_it_cannot_append_a_task_on_bad_payload()
     {
-        Mockery::close();
-        parent::tearDown();
+        $this->validator->method('validate')->willThrowException(new QueueManagerPayloadException('abvgd'));
+        $pushMock = $this->createPartialMock(Eloquent::class, ['push']);
+        $cfgGetMock = $this->createPartialMock(Repository::class, ['get']);
+        $pushMock->expects($this->never())->method('push');
+
+        $task = new CustomQueueTask();
+
+        $this->queueManager->append($task);
+
     }
 }
